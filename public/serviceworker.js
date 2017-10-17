4804115e029c9c49cf0779949347630a2f9295a0
Marcus Amaral Rolim <m4rkux@gmail.com>
2017-10-17 00:33:00 -0200

    [WIP] Capítulo 5

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,11 +1,33 @@
-var CACHE_NAME = 'gih-cache-v4';
+var CACHE_NAME = 'gih-cache-v5';
 var CACHED_URLS = [
-  '/index-offline.html',
+  // Our HTML
+  '/index.html',
+  // Stylesheets
+  '/css/gih.css',
   'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css',
-  '/css/gih-offline.css',
+  'https://fonts.googleapis.com/css?family=Lato:300,600,900',
+  // JavaScript
+  'https://code.jquery.com/jquery-3.0.0.min.js',
+  '/js/app.js',
+  '/js/offline-map.js',
+  // Images
+  '/img/logo.png',
+  '/img/logo-header.png',
+  '/img/event-calendar-link.jpg',
+  '/img/switch.png',
+  '/img/logo-top-background.png',
   '/img/jumbo-background-sm.jpg',
-  '/img/logo-header.png'
+  '/img/jumbo-background.jpg',
+  '/img/reservation-gih.jpg',
+  '/img/about-hotel-spa.jpg',
+  '/img/about-hotel-luxury.jpg',
+  '/img/event-default.jpg',
+  '/img/map-offline.jpg',
+  // Events JSON
+  '/events.json'
 ];
+var googleMapsAPIJS = 'https://maps.googleapis.com/maps/api/js?key='+
+'AIzaSyDm9jndhfbcWByQnrivoaWAEQA8jy3COdE&callback=initMap';
 
 self.addEventListener('install', function (event) {
   event.waitUntil(
@@ -16,17 +38,72 @@ self.addEventListener('install', function (event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    fetch(event.request).catch(function() {
-      return caches.match(event.request).then(function(response) {
-        if (response) {
-          return response;
-        } else if (event.request.headers.get('accept').includes('text/html')) {
-          return caches.match('/index-offline.html');
-        }
-      });
-    })
-  );
+  var requestURL = new URL(event.request.URL);
+  // Handle requests for index.html
+  if (requestURL.pathname === '/' || requestURL.pathname === '/index.html') {
+    event.respondWith(
+      caches.open(CACHE_NAME).then(function (cache) {
+        return cache.match('/index.html').then(function(cachedResponse) {
+          var fetchPromise = fetch('/index.html').then(function(networkResponse) {
+            cache.put('/index.html', networkResponse.clone());
+            return networkResponse;
+          });
+          return cachedResponse || fetchPromise;
+        });
+      })
+    );
+    // Handle requests for Google Maps JavaScript API file
+
+  } else if (requestURL.href === googleMapsAPIJS) {
+    event.respondWith(
+      fetch (
+        googleMapsAPIJS + '&' + Date.now(),
+        { mode: 'no-cors', cache: 'no-store' }
+      ).catch(function() {
+        return caches.match('/js/offline-map.js');
+      })
+    );
+  // Handle Events from JSON file
+  } else if (requestURL.pathname === '/events.json') {
+    event.respondWith(
+      caches.open(CACHE_NAME).then(function (cache) {
+        return fetch(event.request).then(function(networkResponse) {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        }).catch(function () {
+          return caches.match(event.request);
+        });
+      })
+    )
+  // Handle request for images
+  } else if (requestURL.pathname.startsWith('/img/event-')) {
+    event.respondWith(
+      caches.open(CACHE_NAME).then(function (cache) {
+        return cache.match(event.request).then(function (cachedResponse) {
+          return cachedResponse || fetch(event.request).then(function (networkResponse) {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          }).catch(function () {
+            return cache.match('/img/event-default.jpg');
+          });
+        });
+      })
+    );
+  // Handle analytics requests
+  } else if (requestURL.host === 'www.google-analytics.com') {
+    event.respondWith(fetch(event.request));
+  // Handle requests for files cached during installation
+  } else if (
+    CACHED_URLS.includes(requestURL.href) ||
+    CACHED_URLS.includes(requestURL.pathname)) {
+      event.respondWith(
+        caches.open(CACHE_NAME).then(function(cache) {
+          return cache.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+          });
+        })
+      );
+    }
 });
 
 self.addEventListener('activate', function(event) {
